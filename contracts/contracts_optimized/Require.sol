b534cd06b5640d9840092891a60c5bbf42816877
marie-fourier <sobirovutkir@gmail.com>
2023-01-01 16:52:57 +0500

    solution for require.sol

---
+++ contracts/contracts_optimized/Require.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.15;
+
+contract OptimizedRequire {
+    uint256 constant COOLDOWN = 1 minutes;
+    uint256 lastPurchaseTime;
+
+    function purchaseToken() external payable {
+        assembly {
+            if or(
+                iszero(eq(callvalue(), 100000000000000000)), // 0.1 ether
+                iszero(gt(
+                    timestamp(),
+                    add(sload(lastPurchaseTime.slot), 60) // 1 minute
+                ))
+            ) {
+                mstore(0x00, "cannot purchase")
+                revert(0x00, 0x20)
+            }
+            sstore(lastPurchaseTime.slot, timestamp())
+        }
+    }
+}
