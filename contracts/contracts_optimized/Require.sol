ba2dfacf6fe30f36c19192cbd02b7d17ae69a847
marie-fourier <sobirovutkir@gmail.com>
2023-01-01 17:41:54 +0500

    fix error msg

--- contracts/contracts_optimized/Require.sol
+++ contracts/contracts_optimized/Require.sol
@@ -13,8 +13,10 @@ contract OptimizedRequire {
                     add(sload(lastPurchaseTime.slot), 60) // 1 minute
                 ))
             ) {
-                mstore(0x00, "cannot purchase")
-                revert(0x00, 0x20)
+                mstore(0x00, 0x08c379a000000000000000000000000000000000000000000000000000000000)
+                mstore(0x20, 0x0000002000000000000000000000000000000000000000000000000000000000)
+                mstore(0x40, 0x0000000f63616e6e6f7420707572636861736500000000000000000000000000)
+                revert(0x00, 0x64)
             }
             sstore(lastPurchaseTime.slot, timestamp())
         }
